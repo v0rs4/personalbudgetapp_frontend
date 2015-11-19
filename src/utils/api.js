a95d64383da42542f54d38883e2bc3a0ffef39a6
Vladislav Pauk <vladislavpauk@gmail.com>
2015-11-19 12:46:17 +0200

    Clean up utils/api

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,11 +4,6 @@ import fetch from 'isomorphic-fetch';
 
 const {API_BASE_URL} = CONFIG
 
-const ENDPOINT_MAPPER  = {
-  '/api/v1/budget_domains': fetchBudgetDomains,
-  '/oauth/token/info': fetchTokenInfo
-};
-
 function authHeader(accessToken) {
   return {
     'Authorization': `Bearer ${accessToken}`
@@ -33,36 +28,12 @@ function parseJSON(response) {
   return response.json();
 }
 
-export function call(endpoint, {accessToken = undefined, authorization}) {
-  const apiCaller = ENDPOINT_MAPPER[endpoint];
-  if (typeof apiCaller !== 'function') {
-    throw new Error('Endpoint does not exist');
-  }
-  if (authorization === true) {
-    if (typeof accessToken !== 'string') {
-      throw new Error('Access Token required');
-    }
-    return  apiCaller(accessToken);
-  } else {
-    return  apiCaller();
-  }
-}
-
 export function fetchTokenInfo(accessToken) {
   return fetch(`${API_BASE_URL}/oauth/token/info`,{
     headers: Object.assign({}, headers, authHeader(accessToken))
   })
-  .then(response => {
-    if (response.status >= 200 && response.status < 300) {
-      return response;
-    } else {
-      var error = new Error(response.statusText);
-      error.response = response;
-      throw error;
-    }
-  }).then(response => {
-    return response.json();
-  });
+  .then(checkStatus)
+  .then(parseJSON);
 }
 
 export function fetchBudgetDomains(accessToken) {
