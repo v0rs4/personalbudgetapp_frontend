1be851cb59fc62b9056b98816f32befe7dd28340
Vladislav Pauk <vladislavpauk@gmail.com>
2015-11-19 12:45:45 +0200

    Clean up Root component

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,9 +2,6 @@
 import React from 'react';
 
 export default React.createClass({
-  componentDidMount: function(){
-    // this.props.authorizeAccessToken(this.props.accessToken)
-  },
   render: function() {
     return this.props.children;
   }
