8d5e64de045694db54fd8a89bad61a37d5af9849
Vladislav Pauk <vladislavpauk@gmail.com>
2015-11-19 12:38:57 +0200

    Adjust/refactor redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,10 +5,19 @@ import reducer from '../reducer';
 import api from '../middlewares/api';
 import redirect from '../middlewares/redirect';
 import DevTools from '../components/DevTools';
+import Immutable from 'immutable';
 
 const logger = createLogger({
   transformer: (state) => {
-    return state.toJS();
+    var newState = {};
+    for (var i of Object.keys(state)) {
+      if (Immutable.Iterable.isIterable(state[i])) {
+        newState[i] = state[i].toJS();
+      } else {
+        newState[i] = state[i];
+      }
+    };
+    return newState;
   }
 });
 
