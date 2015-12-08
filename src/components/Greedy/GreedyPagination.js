92ceb019d4a3bf9f6c23d4560b31d74fea0a3c21
Vladislav Pauk <vladislavpauk@gmail.com>
2015-12-08 22:22:51 +0200

    Enhance greedy pagination

--- src/components/Greedy/GreedyPagination.js
+++ src/components/Greedy/GreedyPagination.js
@@ -10,26 +10,59 @@ export default React.createClass({
       this.props.changePage(page);
     };
   },
-  renderPaginationList: function() {
+  prevPage: function(e) {
+    e.preventDefault();
+    const { currentPage } = this.props;
+    this.props.changePage(currentPage - 1);
+  },
+  nextPage: function(e) {
+    e.preventDefault();
+    const { currentPage } = this.props;
+    this.props.changePage(currentPage + 1);
+  },
+  renderPrevListItem: function() {
+    const { currentPage } = this.props;
+    return (
+      <li>
+        { currentPage == 1 ?
+          <span className="disabled">Previous</span> :
+          <a href="" onClick={this.prevPage}>Previous</a> }
+      </li>
+    );
+  },
+  renderNextListItem: function() {
     const { currentPage, maxPage } = this.props;
-
-    const list = paginator(currentPage, maxPage).map((n,i) => {
+    return (
+      <li>
+        { currentPage < maxPage ?
+          <a href="" onClick={this.nextPage}>Next</a> :
+          <span className="disabled">Next</span> }
+      </li>
+    );
+  },
+  renderPaginationListItems: function() {
+    const { currentPage, maxPage } = this.props;
+    const list =  paginator(currentPage, maxPage, 3).map((n,i) => {
       let classNames = classnames({active: currentPage === n});
-      return <li className={classNames} key={i}>
-        { n === '...' ?
-          <span>...</span> :
-          <a href="#" onClick={this.changePage(n)}>{n}</a> }
-      </li>;
+      return (
+        <li className={classNames} key={i}>
+          { n === '...' ?
+            <span>...</span> :
+            <a href="#" onClick={this.changePage(n)}>{n}</a> }
+        </li>
+      );
     });
-
-    return <ul className="pagination">{list}</ul>;
+    return [
+      this.renderPrevListItem(),
+      ...list,
+      this.renderNextListItem()
+    ];
+  },
+  renderPaginationList: function() {
+    return <ul className="pagination">{this.renderPaginationListItems()}</ul>;
   },
   renderPagination: function() {
-    return (
-      <nav>
-        {this.renderPaginationList()}
-      </nav>
-    );
+    return <nav>{this.renderPaginationList()}</nav>;
   },
   render: function() {
     return this.renderPagination();
